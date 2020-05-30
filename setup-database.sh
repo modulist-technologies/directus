6a84df25f5d455e6b7f7bcfccabb94bf0ee27ec2
silentfred <cmptrwizard@gmail.com>
2020-05-30 11:13:42 +0200

    initial commit for Kwatani Directus on docker

--- setup-database.sh
+++ setup-database.sh
@@ -3,3 +3,5 @@
 mysql -uroot -e "CREATE DATABASE ${DIRECTUS_DATABASE_NAME};"
 
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${DIRECTUS_DATABASE_USERNAME}'@'localhost' IDENTIFIED BY '$DIRECTUS_DATABASE_PASSWORD';"
+
+echo "Database setup done."
